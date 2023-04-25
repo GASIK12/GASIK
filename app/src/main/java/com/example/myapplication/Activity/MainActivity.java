a4a363c00b41b538b56876310b75c1387bb73eed
Jeongthingamajig <netpetet@naver.com>
2023-04-25 19:52:22 +0900

    feat: activity 따로 분리, map.xml, toolbar.xml 추가

--- app/src/main/java/com/example/myapplication/Activity/MainActivity.java
+++ app/src/main/java/com/example/myapplication/Activity/MainActivity.java
@@ -1,18 +1,40 @@
-package com.example.myapplication;
+package com.example.myapplication.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.room.Room;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.myapplication.R;
+import com.example.myapplication.User;
+import com.example.myapplication.UserDao;
+import com.example.myapplication.UserDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
     private UserDao mUserDao;
+    private Toolbar toolbar;
+    private Button btn_move;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_move = findViewById(R.id.menuButton);
+        // menuActivity로 이동
+        btn_move.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MenuActivity.class);
+                startActivity(intent); // Activity 이동
+            }
+        });
 
         UserDatabase database = Room.databaseBuilder(getApplicationContext(), UserDatabase.class,  "SIKGA")
                 .fallbackToDestructiveMigration()       // 스키마 버전 변경 가능
@@ -28,5 +50,9 @@ public class MainActivity extends AppCompatActivity {
         user.setPhoneNumber("01012345678");
 
         mUserDao.setInsertUser(user);
+
+        //툴바
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
