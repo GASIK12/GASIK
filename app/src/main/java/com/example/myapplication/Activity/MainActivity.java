756bccd5a22ef8c0bdc629872534d2b288c7e09d
Jeongthingamajig <netpetet@naver.com>
2023-04-29 23:56:12 +0900

    refactor: MainActivity에서 roomdb 관련 삭제, room을 이용하는 User클래스와 UserDao삭제

--- app/src/main/java/com/example/myapplication/Activity/MainActivity.java
+++ app/src/main/java/com/example/myapplication/Activity/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.myapplication.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.room.Room;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,13 +9,9 @@ import android.view.View;
 import android.widget.Button;
 
 import com.example.myapplication.R;
-import com.example.myapplication.User;
-import com.example.myapplication.UserDao;
-import com.example.myapplication.UserDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
-    private UserDao mUserDao;
     private Toolbar toolbar;
     private Button btn_move;
 
@@ -27,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btn_move = findViewById(R.id.menuButton);
+
         // menuActivity로 이동
         btn_move.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,21 +31,6 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent); // Activity 이동
             }
         });
-
-        UserDatabase database = Room.databaseBuilder(getApplicationContext(), UserDatabase.class,  "SIKGA")
-                .fallbackToDestructiveMigration()       // 스키마 버전 변경 가능
-                .allowMainThreadQueries()               // Main Thread에서 DB에 IO(Input / Output)를 가능하게 함
-                .build();
-
-        mUserDao = database.userDao(); // 인터페이스 객체 할당
-
-        // 데이터 삽입
-        User user = new User(); // 객체 인스턴스 생성
-        user.setName("김민재");
-        user.setAge("26");
-        user.setPhoneNumber("01012345678");
-
-        mUserDao.setInsertUser(user);
 
         //툴바
         toolbar = findViewById(R.id.toolbar);
